4a8b35d65f5ec50b2da046ef50118b571cd3e24a
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-10-18 21:59:53 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA4UuYyfXx_vElIho7iVc2s4U53PpBUz0w",
+    authDomain: "brand-shop-b9fbd.firebaseapp.com",
+    projectId: "brand-shop-b9fbd",
+    storageBucket: "brand-shop-b9fbd.appspot.com",
+    messagingSenderId: "711967951523",
+    appId: "1:711967951523:web:668617c5acf46732050d16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
