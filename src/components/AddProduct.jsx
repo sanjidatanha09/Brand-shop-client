847c5ebe812c2e8014ea0e3bf7ed038599eb964d
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-15 07:52:17 +0600

    space removed again

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Swal from 'sweetalert2'
 
 const AddProduct = () => {
-
     const handleAddProduct = event =>{
         event.preventDefault();
         const form = event.target;
@@ -13,11 +12,7 @@ const AddProduct = () => {
         const price = form.price.value;
         const description = form.description.value;
         const rating = form.rating.value;
-
         const newProduct= {image, name, brandname,type,price,description,rating}
-
-        // console.log(newProduct);
-
         //send data to the server 
         fetch(' https://brand-shop-rhs6z662f-sanjida-tanhas-projects.vercel.app/product',{
             method:'POST',
@@ -36,16 +31,12 @@ const AddProduct = () => {
                     icon: 'success',
                     confirmButtonText: 'Cool'
                 })
-
             }
         })
-
     }
 
     return (
-        <div className='mt-16 '>
-
-
+        <div className='mt-16'>
             <div className='border text-gray-600 font-bold text-3xl max-w-4xl mx-auto text-center  p-12 bg-[#F4F3F0]'>
                 <h2 className='text-5xl font-bold text-gray-600 mb-5'>Add a Product</h2>
                 <form onSubmit={handleAddProduct}>
@@ -55,7 +46,6 @@ const AddProduct = () => {
                                 <span className="label-text">Image</span>
                             </label>
                             <label className="input-group">
-
                                 <input type="text" name="image" placeholder="Image" className="input input-bordered w-full" />
                             </label>
                         </div>
@@ -64,19 +54,16 @@ const AddProduct = () => {
                                 <span className="label-text">Name</span>
                             </label>
                             <label className="input-group">
-
                                 <input type="text" name="name" placeholder="Name" className="input input-bordered w-full" />
                             </label>
                         </div>
                     </div>
-
                     <div className='flex justify-between items-center gap-6'>
                         <div className="form-control w-3/6">
                             <label className="label">
                                 <span className="label-text">Brand Name</span>
                             </label>
                             <label className="input-group">
-
                                 <input type="text" name="brandname" placeholder="Brand Name" className="input input-bordered w-full" />
                             </label>
                         </div>
@@ -85,19 +72,16 @@ const AddProduct = () => {
                                 <span className="label-text">Type</span>
                             </label>
                             <label className="input-group">
-
                                 <input type="text" name="type" placeholder="Type" className="input input-bordered w-full" />
                             </label>
                         </div>
                     </div>
-
                     <div className='flex justify-between items-center gap-6'>
                         <div className="form-control w-3/6">
                             <label className="label">
                                 <span className="label-text">Price</span>
                             </label>
                             <label className="input-group">
-
                                 <input type="text" name="price" placeholder="Price" className="input input-bordered w-full" />
                             </label>
                         </div>
@@ -125,19 +109,9 @@ const AddProduct = () => {
                         <button className="btn btn-neutral ">Add Product</button>
 
                     </div>
-
-                   
-
-
-
-
                 </form>
-
-
-
             </div>
             <div className='pb-32'>
-
             </div>
         </div>
     );
